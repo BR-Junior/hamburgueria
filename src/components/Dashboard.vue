57e1631c8575453c38893763a2f73339727cc972
BR-Junior <bianor.tech@gmail.com>
2022-11-30 20:48:08 -0300

    refatore

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -9,21 +9,25 @@
                 <div>Ações:</div>
             </div>
             <div class="burger-table-rws">
-                <div class="burger-table-row">
-                    <div class="order-number">1</div>
-                    <div>nome</div>
-                    <div>pão</div>
-                    <div>carne</div>
+                <div class="burger-table-row" v-for="burger in burgers" :key="burgers.id">
+                    <div class="order-number">{{ burger.id }}</div>
+                    <div>{{ burger.nome }}</div>
+                    <div>{{ burger.pao }}</div>
+                    <div>{{ burger.carne }}</div>
                     <div>
                         <ul>
-                            <li>ops</li>
-                            <li>ops</li>
+                            <li v-for="(opcional, index) in burger.opcionais" :key="index" >
+                                {{ opcional }}
+                            </li>
                         </ul>
                     </div>
-                    <select name="status" id="status">
+                    <select name="status" class="status" @change="updateBurger($event, burger.id)">
                         <option value="">Status</option>
+                        <option v-for="s in status" :key="s.id" :value="s.tipo" :selected="burger.status == s.tipo">
+                        {{ s.tipo }}
+                        </option>
                     </select>
-                    <button class="delete-bnt">Cancelar</button>
+                    <button class="delete-bnt" @click="deleteBurger(burger.id)">Cancelar</button>
                 </div>    
             </div>
         </div>
@@ -32,6 +36,48 @@
 </template>
 
 <script setup lang="ts">
+import { ref, onMounted } from 'vue';
+
+const burgers = ref()
+const burger_id = ref()
+const status = ref()
+
+const getPedidos = async () => {
+    const req = await fetch('http://localhost:3000/burgers')
+    const data = await req.json()
+    burgers.value = data
+
+    getStatus()
+    
+}
+
+const getStatus = async () => {
+    const req = await fetch('http://localhost:3000/status')
+    const date = await req.json()
+    status.value = date
+}
+
+const deleteBurger = async (id:number) => {
+    const req = await fetch(`http://localhost:3000/burgers/${id}`, { method: 'DELETE' })
+    const res = await req.json()
+    getPedidos()
+}
+
+const updateBurger = async (event:any, id:number) => {
+    const option = event.target.value
+    const dateJson = JSON.stringify({ status: option })
+    const req = await fetch(`http://localhost:3000/burgers/${id}`, { 
+        method: 'PATCH',
+        headers: { 'content-Type': 'application/json' },
+        body: dateJson
+    })
+    const res = await req.json()
+    console.log(res)    
+}
+
+onMounted(() => {
+    getPedidos()
+})
 
 </script>
 
